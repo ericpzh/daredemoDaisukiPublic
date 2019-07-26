344a3ef223cb900a76aa574d5aa0b983aa742c7e
ericpzh <zp3@illinois.edu>
2019-07-26 14:51:50 -0500

    Delete secret.js

--- daredemoDaisuki/src/secret.js
+++
@@ -1,7 +0,0 @@
-//youtube keys
-export const defaultKeyYoutube = "AIzaSyCNHDQoUdK8hCqx1aWQZYep-O1GC8VNQ9Y";
-//twitter keys
-export const defaultKey = "LItWRhJJel6240WtHI6p7GfY8";
-export const defaultSsecretKey = "2UEsV59GCZjteZR1uok0KygoRE3NwguzRL2xn6eL19h62X8ZmE";
-export const defaultToken = "1125505629290926081-OC5bN6jmnvpDJPHA20WNmHFsLn2UN0";
-export const defaultSecretToken = "9vVPhAm4ChDU0XCXqVRRTsyIToUWTUKZvbXU3NqGpAwZM";
