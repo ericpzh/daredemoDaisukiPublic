2ffb04dca535bc1e7d2eaafbbac9d82a1e57f2f6
ericpzh <zp3@illinois.edu>
2019-08-05 22:43:05 -0500

    firebase hosting

---
+++ vtuber/build/precache-manifest.d7ba055e4a4c5628f7d354ee392a24db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b6be202768d5ba4f0340979105c233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a92ae7e522f479fc5f92",
+    "url": "/static/css/main.1f828283.chunk.css"
+  },
+  {
+    "revision": "2f112a53ffadb78b8015",
+    "url": "/static/js/2.b2762b68.chunk.js"
+  },
+  {
+    "revision": "a92ae7e522f479fc5f92",
+    "url": "/static/js/main.01626f98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "656ece124d7d4dd9675dd7d7ee0d53fb",
+    "url": "/static/media/appBackground1.656ece12.jpg"
+  },
+  {
+    "revision": "6902863644392998c59a81b9ea4e1095",
+    "url": "/static/media/appBackground2.69028636.jpg"
+  },
+  {
+    "revision": "0062f1b761c84d2489fa69d163b90e9d",
+    "url": "/static/media/group.0062f1b7.gif"
+  },
+  {
+    "revision": "b270a2ca72239219bb3502a40e8abd9a",
+    "url": "/static/media/home.b270a2ca.gif"
+  },
+  {
+    "revision": "011727d7150f71bbd776404ae853a401",
+    "url": "/static/media/load1.011727d7.png"
+  },
+  {
+    "revision": "b50cc99af075bb39f297050dd49f78f9",
+    "url": "/static/media/load2.b50cc99a.png"
+  },
+  {
+    "revision": "791aa32205e1884759f6d81450b852b0",
+    "url": "/static/media/load3.791aa322.png"
+  },
+  {
+    "revision": "6f0df9b171bbde7bb5563c981cf3f627",
+    "url": "/static/media/load4.6f0df9b1.png"
+  },
+  {
+    "revision": "f3745460f12fae69d1ceba757f433c82",
+    "url": "/static/media/load5.f3745460.png"
+  },
+  {
+    "revision": "5238519089a74b834b24aa5de0886fcd",
+    "url": "/static/media/load6.52385190.png"
+  },
+  {
+    "revision": "118da176d84877a617aea5a3574094a4",
+    "url": "/static/media/register.118da176.gif"
+  },
+  {
+    "revision": "9e8c02d40bf6e6fc708bf453f0acef0d",
+    "url": "/static/media/search.9e8c02d4.gif"
+  },
+  {
+    "revision": "0d10c24c7b66fc40ec541c160e133009",
+    "url": "/static/media/subscriptions.0d10c24c.gif"
+  },
+  {
+    "revision": "dd6e5b7b7a2a0a586ed8b64edec2c5a9",
+    "url": "/static/media/suggestion.dd6e5b7b.gif"
+  }
+]);
